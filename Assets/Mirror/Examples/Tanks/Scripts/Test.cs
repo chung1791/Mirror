bef252829a177d542203a680abfcfb1ea0e9e524
vis2k <info@noobtuts.com>
2021-09-28 17:43:42 +0800

    remove comparison

--- Assets/Mirror/Examples/Tanks/Scripts/Test.cs
+++ Assets/Mirror/Examples/Tanks/Scripts/Test.cs
@@ -3,6 +3,5 @@ namespace Mirror.Examples.Tanks
     public class Test : NetworkBehaviour
     {
         [SyncVar] public int health = 42;
-         public SyncVar<int> yo = new SyncVar<int>(42);
     }
 }
