176b0889e3d5808f4e5d3bf21e9e2a914faf951c
vis2k <info@noobtuts.com>
2021-09-28 15:08:41 +0800

    test

--- Assets/Mirror/Examples/Tanks/Scripts/Test.cs
+++ Assets/Mirror/Examples/Tanks/Scripts/Test.cs
@@ -2,6 +2,7 @@ namespace Mirror.Examples.Tanks
 {
     public class Test : NetworkBehaviour
     {
+        [SyncVar] public int health = 42;
         public SyncVar<int> yo = new SyncVar<int>(42);
     }
 }
